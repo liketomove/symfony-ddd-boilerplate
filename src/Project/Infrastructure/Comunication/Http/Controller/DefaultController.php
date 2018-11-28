b13bcf7edd242a7f5ae9362bfd0b9a2d8a79e7e1
Matiux <m.galacci@gmail.com>
2018-11-28 11:03:14 +0100

    Sistemazione conf docker + namespace sf

--- src/Project/Infrastructure/Comunication/Http/Controller/DefaultController.php
+++ src/Project/Infrastructure/Comunication/Http/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Project\Infrastructure\Comunication\Http\Controller;
+namespace Project\Infrastructure\Comunication\Http\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
